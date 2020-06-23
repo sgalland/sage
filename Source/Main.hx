5d7db8f8e8a14fea0033080d1cab10f6c4c31a3d
Sean Galland <sgalland@timeshareware.com>
2020-06-23 13:40:23 -0600

    New updates
    Added full DCE (reduce compile times) and cleaned up Main to reflect a proper entry to the AGI Interpreter. Also removed .haxerc for lix since it doesn't work right with OpenFL.

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,47 +1,42 @@
 package;
 
+import sage.agi.AGIResourceType;
+import sage.agi.AGIFileNameTools;
 import sage.agi.helpers.AGIColor;
 import openfl.geom.Rectangle;
 import openfl.display.BitmapData;
 import openfl.display.Bitmap;
 import sage.agi.resources.AGIFileReader;
-import sage.agi.EAGIFileName;
 import openfl.display.Sprite;
 import sage.agi.resources.AGIView;
+import sage.agi.resources.AGIPicture;
+import sage.agi.interpreter.AGIInterpreter;
 
 class Main extends Sprite {
-	var drawable:AGIView;
+	var drawable:AGIPicture;
 
 	public function new() {
 		super();
 
 		lime.app.Application.current.window.title = "SAGE";
 
-		var interpreter = new sage.agi.interpreter.AGIInterpreter.AGIInterpreter();
+		while (true) {
+			AGIInterpreter.instance.run();
+		}
 
-		// Sys.setCwd("C:\\Users\\sgalland\\Desktop\\AGIDATA\\DOS\\AGI2\\BC_2_439");
-		var reader = new AGIFileReader();
-		// reader.loadDirectoryEntries(EAGIFileName.LOGIC);
-		// var file = reader.getFile(0);
-		// var logic = new AGILogic(file, 0);
-		// trace(logic.getMessage(0));
-
-		reader.loadDirectoryEntries(EAGIFileName.VIEW);
-		var file = reader.getFile(0);
-
-		drawable = new AGIView(file);
-		var target:ViewCell = drawable.getViewLoops()[0].loopCells[0];
-
+		// TODO: Keep this for later
 		// https://www.openfl.org/learn/npm/api/classes/openfl.display.bitmapdata.html#setpixels
 		// https://help.adobe.com/en_US/FlashPlatform/reference/actionscript/3/flash/display/BitmapData.html#setPixels()
-		try {
-			AGIColorConverter.convertPixelsToRGB(target.data);
-			render(this, 0, 0, target.width, target.height, target.data, target.transparentColor);
-			render(this, target.width * 4 + 10, 0, target.width, target.height, target.data, target.transparentColor);
-			render(this, target.width * 8 + 20, 0, target.width, target.height, target.data, target.transparentColor);
-		} catch (e:String) {
-			trace(e);
-		}
+		// try {
+		// 	var data:Array<Int> = [for (i in drawable.getPicturePixels()) i];
+
+		// 	AGIColorConverter.convertPixelsToRGB(data);
+		// 	render(this, 0, 0, 320, 200, data, AGIColor.getColorByDosColor(15));
+		// 	// render(this, target.width * 4 + 10, 0, target.width, target.height, target.data, target.transparentColor);
+		// 	// render(this, target.width * 8 + 20, 0, target.width, target.height, target.data, target.transparentColor);
+		// } catch (e:String) {
+		// 	trace(e);
+		// }
 	}
 
 	static function render(stage:Sprite, x:Int, y:Int, width:Int, height:Int, bytes:Array<Int>, transparentColor:AGIColor) {
@@ -58,8 +53,8 @@ class Main extends Sprite {
 		data.setPixels(rect, byteArray);
 
 		var bitmap = new Bitmap(data);
-		bitmap.scaleX = 4;
-		bitmap.scaleY = 4;
+		// bitmap.scaleX = 4;
+		// bitmap.scaleY = 4;
 		bitmap.x = x;
 		bitmap.y = y;
 		stage.addChild(bitmap);
